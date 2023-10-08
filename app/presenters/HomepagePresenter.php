22abbaba70e80a3af068d8be530b86a192bbe136
Veronika Civínová <Civinova.Veronika@gmail.com>
2023-10-08 11:51:38 +0200

    feature: simple translation form

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -5,7 +5,31 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use Nette;
+use Nette\Application\UI\Form;
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+	protected function createComponentTranslationForm(): Form
+	{
+		$form = new Form();
+
+		$english = $form->addTextArea('english', 'English text:')
+			->setRequired('Enter your text')
+			->setEmptyValue('Enter your text');
+
+		$pigLatin = $form->addTextArea('pigLatin', 'Pig latin text:')
+			->setDisabled()
+			->setDefaultValue('Translated text will be here');
+
+		$form->addSubmit('translate', 'Translate');
+
+		$form->onSuccess[] = static function () use ($english, $pigLatin): void {
+			/** @var string $englishText */
+			$englishText = $english->getValue();
+			$translatedText = sprintf('translated "%s"', $englishText);
+			$pigLatin->setDefaultValue($translatedText);
+		};
+
+		return $form;
+	}
 }
